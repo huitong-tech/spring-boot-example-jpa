2fbddf58ca738d8e75fc251d0d75d6635f9ded83
thisiswangle <wangle.tju@gmail.com>
2018-10-20 00:04:00 +0800

    make AdminLTE workable

--- src/main/java/com/pivaiot/starter/project/web/controller/UserController.java
+++ src/main/java/com/pivaiot/starter/project/web/controller/UserController.java
@@ -5,11 +5,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/")
-public class HomeController {
+@RequestMapping("/users")
+public class UserController {
 
     @GetMapping
-    public String showHome() {
-        return "dashboard/blank";
+    public String list() {
+        return "user/list-user";
     }
+
 }
