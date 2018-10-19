e3ba688e4b6e4d8f5d357ad0191232f8d417b242
thisiswangle <wangle.tju@gmail.com>
2018-10-19 12:49:29 +0800

    dto 添加构造函数

--- src/main/java/com/pivaiot/starter/project/data/hello/Greeting.java
+++ src/main/java/com/pivaiot/starter/project/data/hello/Greeting.java
@@ -1,12 +1,24 @@
 package com.pivaiot.starter.project.data.hello;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
 
 @Data
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class Greeting {
 
     private String says;
+
+    private String lang = "zh-CN";
+
+    private Date createdTime;
+
+    private Date updatedTime;
 
 }
