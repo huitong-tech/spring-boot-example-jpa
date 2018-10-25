309aa03cfb94fa5ad7df1cb6ba1060ca0d02c431
thisiswangle <wangle.tju@gmail.com>
2018-10-25 11:33:58 +0800

    rename file upload directory

--- src/main/java/com/pivaiot/starter/project/config/FileUploadConfig.java
+++ src/main/java/com/pivaiot/starter/project/config/FileUploadConfig.java
@@ -6,16 +6,16 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ConfigurationProperties(prefix = "fileupload")
+@ConfigurationProperties(prefix = "upload")
 public class FileUploadConfig {
     /**
-     * Folder for storing upload files
+     * Directory for storing upload files
      */
     @Getter @Setter
-    private String path = "./files";
+    private String dir = "./files";
 
     @Getter @Setter
-    private String tmpPath = "./files/tmp";
+    private String tmpDir = "./files/tmp";
 
     @Getter @Setter
     private Long maxFileSize = 10 * 1024L * 1024L; // 10MB
