4cde12bc5412421a5a5106f7caa6e67907485371
thisiswangle <wangle.tju@gmail.com>
2018-12-01 15:00:53 +0800

    rename test entry point

--- src/test/java/com/pivaiot/example/jpa/ExampleJpaApplicationTests.java
+++ src/test/java/com/pivaiot/example/jpa/ExampleJpaApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class StarterProjectApplicationTests {
+public class ExampleJpaApplicationTests {
 
 	@Test
 	public void contextLoads() {
